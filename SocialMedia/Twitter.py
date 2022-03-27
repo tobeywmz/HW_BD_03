b7e658c35f31ec809ef8d5d6b5ddf8ac58103a02
Omid Reza <omid77.orh@gmail.com>
2022-03-27 06:27:32 +0430

    Fix code

--- SocialMedia/Twitter.py
+++ SocialMedia/Twitter.py
@@ -2,32 +2,14 @@ from SocialMedia import SocialMedia
 
 class Twitter(SocialMedia):
     
-  def __init__(self, name, body):
-      super().__init__(name)
-      slef.body = body
-      
-Tweets= []
+    def __init__(self):
+        super().__init__("Twitter")
+        self.Tweets= []
 
+    def CreateNewTweet(self, body):
+        
+        if len(body)<280:
+            self.Tweets.append(body)
 
-def CreateNewTweet(Tweet):
-    print('Enter the body of your new tweet:')
-    body = input()
-
-    if len(body)<280:
-        Tweets.append(body)
-
-def GetTweets():
-    return self.Tweets
-
-i=0
-while i<1:
-    print('Do you want to create a new tweet?(yes/no)')
-    answer = input()
-      
-    if answer == 'yes':
-          CreateNewTweet(Tweets)
-          
-    elif answer == 'no':
-          print('your tweets are:')
-          print(Tweets)
-          i= i+1
+    def GetTweets(self):
+        return self.Tweets
